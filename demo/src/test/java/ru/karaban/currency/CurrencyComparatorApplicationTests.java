5fe287ac86bdab9b924f0a9170cc6cbcd68a62c0
gleb1524 <gleb1524@yandex.ru>
2022-06-02 19:53:15 +0300

    Initial commit

---
+++ demo/src/test/java/ru/karaban/currency/CurrencyComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.karaban.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
